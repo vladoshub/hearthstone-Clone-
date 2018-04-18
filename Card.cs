ef590a4e4c7477a94509b0589f0cfc8ca15cfc04
vladoshub <38497674+vladoshub@users.noreply.github.com>
2018-04-18 15:48:37 +0300

    Add files via upload

---
+++ Card.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+
+using System.Globalization;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace Карты
+{
+    
+
+
+    public partial class RootObject
+    {
+        
+        [JsonProperty("UserId")]
+        public string UserId { get; set; }
+
+        [JsonProperty("Cards")]
+        public Card[] Cards { get; set; }
+
+        public RootObject(string name, Card[] Tx)
+        {
+            UserId = name;
+            Cards = Tx;
+
+        }
+    }
+
+    public partial class Card
+    {
+        public Card(int hp,int mana,int type,string name,int damage)
+        {
+            Hp = hp;
+            Mana = mana;
+            Type = type;
+            Name = name;
+            Damage = damage;
+
+        }
+
+
+        [JsonProperty("hp")]
+        public int Hp { get; set; }
+
+        [JsonProperty("mana")]
+        public int Mana { get; set; }
+
+        [JsonProperty("type")]
+        public int Type { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("damage")]
+        public int Damage { get; set; }
+    }
+
+   
+}
---
+++ GenericOnClientJsonFile.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace Карты
+{
+    class GenericOnClientJsonFile
+    {
+
+        
+        static void Main(string[] args)
+        {
+
+
+           string name;
+           int Hp, Damage, Mana, Type;
+
+            Card[] C2 = new Card[2];
+            for (int j = 0; j < 2; j++)
+            {
+                Console.WriteLine("Name");
+            name = Console.ReadLine();
+            Console.WriteLine("hp");
+            Hp = int.Parse(Console.ReadLine());
+            Console.WriteLine("Damage");
+            Damage = int.Parse(Console.ReadLine());
+            Console.WriteLine("Mana");
+            Mana = int.Parse(Console.ReadLine());
+            Console.WriteLine("Type");
+            Type = int.Parse(Console.ReadLine());
+            Console.WriteLine("hp");
+
+                Card C = new Card(Hp,Mana,Type,name,Damage);
+                C2[j] = C;
+
+            }
+            RootObject R = new RootObject("OnePerson", C2);
+            Serialize(R);
+        }
+
+
+        public static void Serialize(RootObject r)
+        {
+
+            
+            
+            File.WriteAllText(@"D:\Kaloda.json", JsonConvert.SerializeObject(r));
+
+            
+            using (StreamWriter file = File.CreateText(@"D:\Kaloda.json"))
+            {
+                JsonSerializer serializer = new JsonSerializer();
+                serializer.Serialize(file, r);
+            }
+
+
+        }
+
+    }
+}
